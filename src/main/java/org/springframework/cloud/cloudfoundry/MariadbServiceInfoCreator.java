c0e590fee5c5647fb6d6660ba8af6323b54052e8
myminseok <myminseok@users.noreply.github.com>
2016-11-16 01:13:22 +0900

    Update MariadbServiceInfoCreator.java

--- src/main/java/org/springframework/cloud/cloudfoundry/MariadbServiceInfoCreator.java
+++ src/main/java/org/springframework/cloud/cloudfoundry/MariadbServiceInfoCreator.java
@@ -3,15 +3,10 @@ package org.springframework.cloud.cloudfoundry;
 import org.springframework.cloud.service.common.MariadbServiceInfo;
 import org.springframework.cloud.cloudfoundry.Tags;
 
-/**
- *
- * @author Ramnivas Laddad
- *
- */
+
 public class MariadbServiceInfoCreator extends RelationalServiceInfoCreator<MariadbServiceInfo> {
 
     public MariadbServiceInfoCreator() {
-        // the literal in the tag is CloudFoundry-specific
         super(new Tags("mariadb"), MariadbServiceInfo.MARIADB_SCHEME);
     }
 
